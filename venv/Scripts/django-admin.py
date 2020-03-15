4b5f0aa98af57db062c2a2f38032c6f85cec90b5
Iman Anooshehpour <imananoosheh@users.noreply.github.com>
2020-03-15 17:09:44 -0400

    adding Django template code to html codes

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\nader\PycharmProjects\Catchup\venv\Scripts\python.exe
+#!C:\_maFiles\Documents\Study in Canada\UWindsor\SEM1-Winter2020\[ASET] Advanced Software Engineering Topics\CatchUp App\Catchup\venv\Scripts\python.exe
 from django.core import management
 
 if __name__ == "__main__":
