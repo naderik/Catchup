088d5781792728046c34cd71e6918680f5fc33a2
Kasra Naderi <naderi.kasra@gmail.com>
2020-03-22 16:10:44 -0400

    edit urls

--- user/views.py
+++ user/views.py
@@ -2,6 +2,16 @@ from django.http import HttpResponse
 from django.template import loader
 
 
-def index(request):
+def location(request):
     template = loader.get_template('location.html')
+    return HttpResponse(template.render())
+
+
+def login(request):
+    template = loader.get_template('signin.html')
+    return HttpResponse(template.render())
+
+
+def register(request):
+    template = loader.get_template('register.html')
     return HttpResponse(template.render())
